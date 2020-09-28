ff0e525a63bca550f18ef4acfea31aff4e64761f
dvernier <dvernier@vernier.com>
2020-09-28 11:11:13 -0700

    this has the new, organized version of the Mask code. Why is the serial version not dealt with in this commit?

--- examples/GDXMaskWithI2CDisplay/GDXMaskWithI2CDisplay.ino
+++ examples/GDXMaskWithI2CDisplay/GDXMaskWithI2CDisplay.ino
@@ -1,25 +1,45 @@
-/*Arduino  Mask program for I2C
- A lines with the major indents are for using a 2-line display
- with an I2C connection. Leave commented out what you do not need. 
+/*
+ GDX Mask Demo (v. 20200728, using the 0.85 library)
+ // This version supports a I2C 2-line display
  */
-
 #include "ArduinoBLE.h"
 #include "GDXLib.h"
 GDXLib GDX;
-char phrase1[16];//16 long
-char phrase2[16];
-char strBuffer[32]="unassigned";
-char strUnits[32]="strUnits";
+#define DISPLAY //comment out for no DISPLAY 
    //indented lines are for 2-line display
    // this version uses I2C connection
    #include <Wire.h>
    #include <SerLCD.h> //Click here to get the library: http://librarymanager/All#SparkFun_SerLCD
    SerLCD lcd; // Initialize the library with default I2C address 0x72
+char *phrase1[] = {"1234567890123456",
+                   "This is display ", 
+                   "fun project for ", 
+                   "     Yes        ",
+                   "Thank You       ", 
+                   "Hello, my name  ", 
+                   "How are you     ",
+                   "     No         ",
+                   "Keep wearing    ",
+                    };
 
+char *phrase2[] = {"1234567890123456",
+                   "mask, it is a   ", 
+                   "students to do  ", 
+                   "  ++++++++++    ",
+                   "  very much!    ", 
+                   " Dave Vernier   ", 
+                   "doing today?    ",
+                   "thank you       ",
+                   "  your mask!    ",
+                    };
 void setup(){
   Serial.begin(9600);
-  delay(1000);
+  delay(500);
+  Serial.println("Searching for");
+  Serial.println("GDX Sensor");
+  #if defined DISPLAY
   // 2-LINE DISPLAY CODE
+      // 2-LINE DISPLAY CODE
       Wire.begin();
       lcd.begin(Wire); //Set up the LCD for I2C communication
       lcd.setBacklight(255, 255, 255); //Set backlight to bright white
@@ -30,103 +50,70 @@ void setup(){
       lcd.print("GDX Sensor");
       delay(1000);
       //2-LINE DISPLAY CODE*/
-  Serial.println("Arduino Talking Mask Program");
-  Serial.println("Venier S&T");
-  delay(2000);
-  Serial.println("Searching for GDX sensor...");
-  //GDX.Begin();  //use this line for proximity pairing
-  //or
-  GDX.Begin("GDX-ACC 0H1019K1",1, 500);//or specify device, channel and period here 
+  #endif //DISPLAY
 
-  delay(1000);
-  Serial.print("Found: ");
-  Serial.println(GDX.deviceName());
-  Serial.println(GDX.channelName());
-  Serial.println(GDX.channelUnits());
+  //GDX.Begin();  //use this line for proximity pairing
+    //or
     // 2-LINE DISPLAY CODE
-      lcd.setCursor(0,0);//top-left corner
+      lcd.clear();
+      lcd.print("Searhing for: ");
+      lcd.setCursor(0,1);// column, row
+      lcd.print("GDX-ACC 0H101767");
+      delay(2000);
+  GDX.Begin("GDX-ACC 0H101767",11, 800);//or specify device, channel and period here 
+  Serial.println("Found: ");
+  Serial.print("deviceName:: ");
+  Serial.println(GDX.deviceName());
+  Serial.print("channelName:: ");
+  Serial.println(GDX.channelName());
+  Serial.print("channelUnits:");
+  Serial.println(GDX.channelUnits());
+  Serial.println("---");
+  Serial.print("GetBatteryStatus()");
+  Serial.println(GDX.batteryPercent());
+  Serial.println("---");
+// 2-LINE DISPLAY CODE
+      lcd.clear();
       lcd.print("Found: ");
       lcd.setCursor(0,1);// column, row
       lcd.print(GDX.deviceName());
       delay(2000);
       
-      lcd.clear();
-      lcd.print(GDX.channelName());
-      lcd.setCursor(0,1);// column, row
-      lcd.print(GDX.channelUnits());
-      delay(2000);
-            
-      lcd.clear();
-      sprintf(strBuffer,"%s %d %s %d","batt:",GDX.batteryPercent(),"CS:",GDX.chargeState());
-      lcd.print(strBuffer);
-      sprintf(strBuffer,"%s %d", "channelNumber",GDX.channelNumber());
-      lcd.setCursor(0,1);// column, row
-      lcd.print(strBuffer);
-      delay(2000);
-     
-      lcd.clear();
-      sprintf(strBuffer,"%s %d", "   RSSI:   ",GDX.RSSI());
-      lcd.print(strBuffer);
-      lcd.setCursor(0,1);// column, row
-      lcd.print("threshold = -44 ");//!!!NOTE SET IN .CPP CODE
-      delay(2000);
       //2-LINE DISPLAY CODE*/ 
-      GDX.start();
-}
+  GDX.start();
+  
+}//end of setup
 
-void loop(){
+void loop()
+{
   char strBuffer[32];
-  Serial.print("buffer declared ");
+  int i=0;
+  Serial.println("buffer declared ");
   float channelReading=0;
   channelReading =GDX.readSensor();
-  Serial.println(GDX.channelUnits());
   Serial.print("channelReading: ");
   Serial.println(channelReading);
-   int i=3;
-    if (channelReading>4)
-      i=1;
-   if (channelReading<-4)
-      i=2;
-   if (channelReading>7)
-      i=5;
-   if (channelReading<-7)
-      i=4;
-    Serial.print("i ");
-    Serial.println(i);
-    switch(i){
-    case 1:
-      strcpy(phrase1,"   Hello, My    ");
-      strcpy(phrase2,"  Name is Dave  ");
-      break;
-    case 2:
-      strcpy(phrase1,"   Thank You    ");
-      strcpy(phrase2," For Your Help! ");
-      break;
-     case 3:
-      strcpy(phrase1,"Good Bye, keep  ");
-      strcpy(phrase2,"wearing a mask! ");
-      break;
-     case 4:
-      strcpy(phrase1,"   No           ");
-      strcpy(phrase2," Thank You      ");
-      break;
-     case 5:
-      strcpy(phrase1,"   Yes          ");
-      strcpy(phrase2,"                ");
-      break;
-    }
+  if (abs(channelReading)>157.5) i=1;
+  else if(channelReading>112.5) i=2;
+  else if(channelReading>67.5) i=3;
+    else if (channelReading>22.5) i=4;
+    else if (channelReading>-22.5) i=5;
+    else if (channelReading>-67.5) i=6;
+    else if (channelReading>-112.5) i=7;
+    else  i=8;
  
-  Serial.println(phrase1);
-  Serial.println(phrase2);
+  Serial.println("i ");
+  Serial.println(i);
+  Serial.println("phrase");
+  Serial.println(phrase1[i]);
+  Serial.println(phrase2[i]);
+  
+  #if defined DISPLAY
        // 2-LINE DISPLAY CODE
        lcd.clear(); //Clear 
-       sprintf(strBuffer, "%.2f", channelReading);
-       lcd.print(phrase1);
-       lcd.setCursor(0,1);// column, row
-       lcd.print(phrase2);
+       lcd.print(phrase1[i]);
+       lcd.print(phrase2[i]);
        //2-LINE DISPLAY CODE */
-  delay (1000);
-  
-}
-
-      
+  #endif //DISPLAY
+  delay (800);
+}//end of loop
